6c0785c6f9dd382b8a240462585aa01ca43ff3b8
Rick1970 <rthornbrug@gmail.com>
2016-08-05 10:40:29 -0700

    populate intial files with setup info; run dnu restore

---
+++ Modules/HomeModule.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+using Nancy;
+using Nancy.ViewEngines.Razor;
+using System;
+
+namespace MusicBusiness
+{
+  public class HomeModule : NancyModule
+  {
+    public HomeModule()
+    {
+
+    }
+  }
+}
---
+++ Objects/bands.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace MusicBusiness
+{
+  public class Band
+  {
+    private int _id;
+    private string _name;
+
+    public Book (string Name, int Id = 0)
+    {
+      _id = Id;
+      _title = Name;
+    }
+
+    public static void DeleteAll()
+   {
+     SqlConnection connection = DB.Connection();
+     connection.Open();
+     SqlCommand command = new SqlCommand("DELETE FROM bands;", connection);
+     command.ExecuteNonQuery();
+     connection.Close();
+   }
+
+  }
+}
