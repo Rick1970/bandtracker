b9a30cc9540adeb57e3114ca140a13a7502dbc3b
Rick1970 <rthornbrug@gmail.com>
2016-08-05 17:07:03 -0700

    add band page, and venue page with basic structure for each page.

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -12,6 +12,14 @@ namespace MusicBusiness
        Get ["/"] = _ => {
          return View["index.cshtml"];
        };
+       Get["/venues"] = _ => {
+       List<Venue> AllVenues = Venue.GetAll();
+       return View["venues.cshtml", AllVenues];
+     };
+      Get["/bands"] = _ => {
+       List<Band> AllBands = Band.GetAll();
+       return View["bands.cshtml", AllBands];
+     };
     }
   }
 }
