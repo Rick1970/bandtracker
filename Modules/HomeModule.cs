7ee79a1df303143464127beeb788ae62eef61f33
Rick1970 <rthornbrug@gmail.com>
2016-08-05 19:10:27 -0700

    add form entry page for venues, and band.  Added success page.

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -10,6 +10,7 @@ namespace MusicBusiness
     public HomeModule()
     {
        Get ["/"] = _ => {
+         List<Venue> AllVenues = Venue.GetAll();
          return View["index.cshtml"];
        };
        Get["/venues"] = _ => {
@@ -20,6 +21,24 @@ namespace MusicBusiness
        List<Band> AllBands = Band.GetAll();
        return View["bands.cshtml", AllBands];
      };
+     Get["/venues/new"] = _ => {
+       return View["venues_form.cshtml"];
+     };
+     Post["/venues/new"] = _ =>{
+       Venue newVenue = new Venue(Request.Form["venue-name"]);
+       newVenue.Save();
+       return View["success.cshtml"];
+     };
+     Get["/bands/new"] = _ => {
+       List<Venue> AllVenue = Venue.GetAll();
+       return View["bands_form.cshtml", AllVenue];
+     };
+     Post["/bands/new"] = _ => {
+       Band newBand = new Band(Request.Form["band-name"], Request.Form["venue-id"]);
+       newBand.Save();
+       return View["success.cshtml"];
+     };
+    
     }
   }
 }
